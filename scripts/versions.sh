6f4bf997c1a5ee4986f91445bd0ab6928d030502
quocphu <quocphu@users.noreply.github.com>
2022-04-22 09:37:02 +0700

    Pump version to v1.0.3

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -4,3 +4,6 @@
 subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.2.2'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.7.10'}
+
+# swimmer version
+swimmer_version='v1.0.3'
